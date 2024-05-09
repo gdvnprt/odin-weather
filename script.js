882258395c112b20ba1035d48a3f11e5bb44bbbb
Gavin Davenport <gavinkdavenport@gmail.com>
2024-05-08 20:01:02 -0700

    Begin building data filter
    have an object that is only the data I need from the API call

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function searchWeather(location) {
         .then(function(response) {
             return response.json();
         })
-        // Filter out info I want here into new json object
+        // Filter out info I want here into javascript object
+        // name, region, country
         // current.condition.text
         // current.temp_f and temp_c
         // current.feelslike_f and feelslike_c
@@ -24,3 +25,23 @@ function searchWeather(location) {
 
 // Do DOM stuff with JSON object
 // do a GIFY API pull with current.condition.text
+
+// Filtered object:
+// location > name + region + country
+// time > localtime + lastupdated
+// condition text
+// temp > F or C > temp + feelslike
+// humidity
+// wind > dir + speed > mph + kph
+
+function filterWeather(jsonObject) {
+    //do things like parsing here
+    let location = { place, region, country };
+    let time = { local, lastUpdated };
+    let faren = { deg_f, feelslike_f };
+    let cels = { deg_c, feelslike_c };
+    let temp = { faren, cels };
+    let windSpeed = { mph, kph };
+    let wind = { direction, windSpeed }
+    return { location, time, condition, temp, humidity, wind };
+};
